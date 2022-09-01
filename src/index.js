2025bd51664b2c8877f27517d17bd3c55fc12a88
Yossef Rawy <yossefrwy@gamil.com>
2022-09-01 23:33:30 +0200

    4 steps down

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
+import './style.css';
+import food from'./images.jpg';
+
+
 const divContent = document.getElementById('content')
 
 
 console.log(divContent)
+
+
+const myIcon = new Image();
+myIcon.src = food;
+
+document.body.appendChild(myIcon)
