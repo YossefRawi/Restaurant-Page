df738f690cd1499aed6f2df71b1ac476a9fa8829
Yossef Rawy <yossefrwy@gamil.com>
2022-09-02 16:54:13 +0200

    5 done

---
+++ src/home.js
@@ -0,0 +1,31 @@
+import food from'./images.jpg';
+
+const homeFunction = () => {
+
+
+const divContent = document.getElementById('content')  
+
+//Header
+const header = document.createElement('h1');
+header.classList.add('header');
+header.innerText = "Scriptaurant";
+divContent.appendChild(header);
+
+
+//Home image
+const myIcon = new Image();
+myIcon.classList.add('foodpic')
+myIcon.src = food;
+divContent.appendChild(myIcon);
+
+//dummy Text for home page
+const dummyText = document.createElement('div');
+dummyText.classList.add('dummy');
+dummyText.innerText = 'Best pizza ever made';
+divContent.appendChild(dummyText);
+
+return divContent;
+}
+
+export {homeFunction};
+
--- src/index.js
+++ src/index.js
@@ -1,30 +1,13 @@
 import './style.css';
-import food from'./images.jpg';
+
+import { homeFunction } from './home';
 
 
-const divContent = document.getElementById('content')
 
 
-console.log(divContent)
-
-//Header
-const header = document.createElement('h1');
-header.classList.add('header');
-header.innerText = "Scriptaurant";
-divContent.appendChild(header);
 
 
-//Home image
-const myIcon = new Image();
-myIcon.classList.add('foodpic')
-myIcon.src = food;
-divContent.appendChild(myIcon);
 
-//dummy Text for home page
-const dummyText = document.createElement('div');
-dummyText.classList.add('dummy');
-dummyText.innerText = 'Best pizza ever made';
-divContent.appendChild(dummyText);
-
+homeFunction();
 
 document.body.appendChild(divContent)
